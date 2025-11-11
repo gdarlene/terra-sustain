3b0a9e021a7ace36851e1c3a2023e8057a7e4d6a
gdarlene <darlyganza@gmail.com>
2025-11-11 23:27:19 +0200

    added registration response from the backend

--- backend/TerraSustain/src/main/java/org/inganji/TerraSustain/model/DTO/RegisterResponse.java
+++ backend/TerraSustain/src/main/java/org/inganji/TerraSustain/model/DTO/RegisterResponse.java
@@ -1,4 +1,19 @@
 package org.inganji.TerraSustain.model.DTO;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RegisterResponse {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String username;
 }
