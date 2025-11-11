8a8a7686e85d0567ad79b3100203ced6111292e6
gdarlene <darlyganza@gmail.com>
2025-11-11 21:16:54 +0200

    initial commit

---
+++ backend/TerraSustain/src/main/java/org/inganji/TerraSustain/TerraSustainApplication.java
@@ -0,0 +1,13 @@
+package org.inganji.TerraSustain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraSustainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraSustainApplication.class, args);
+	}
+
+}
---
+++ backend/TerraSustain/src/test/java/org/inganji/TerraSustain/TerraSustainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inganji.TerraSustain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraSustainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
