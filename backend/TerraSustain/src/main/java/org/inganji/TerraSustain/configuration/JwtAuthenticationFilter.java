bfd10b9ce9290ccbaafe2d1631069cac95da434c
gdarlene <darlyganza@gmail.com>
2025-11-12 07:29:32 +0200

    injected dependencies

--- backend/TerraSustain/src/main/java/org/inganji/TerraSustain/configuration/JwtAuthenticationFilter.java
+++ backend/TerraSustain/src/main/java/org/inganji/TerraSustain/configuration/JwtAuthenticationFilter.java
@@ -1,4 +1,25 @@
 package org.inganji.TerraSustain.configuration;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Data;
+import org.inganji.TerraSustain.service.impl.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+@Data
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
