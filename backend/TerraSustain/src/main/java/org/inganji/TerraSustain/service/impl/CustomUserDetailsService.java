90c9fe0dd34624b4a0c298cebb32fe4558263866
gdarlene <darlyganza@gmail.com>
2025-11-12 07:34:48 +0200

    added custom user details service

--- backend/TerraSustain/src/main/java/org/inganji/TerraSustain/service/impl/CustomUserDetailsService.java
+++ backend/TerraSustain/src/main/java/org/inganji/TerraSustain/service/impl/CustomUserDetailsService.java
@@ -1,4 +1,29 @@
 package org.inganji.TerraSustain.service.impl;
 
-public class CustomUserDetailsService {
+import org.inganji.TerraSustain.configuration.CustomUserDetails;
+import org.inganji.TerraSustain.model.Person;
+import org.inganji.TerraSustain.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private PersonRepository personRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Person> person= personRepo.findByUsernameOrPhoneOrEmail(username);
+        if(person.isPresent()){
+            return new CustomUserDetails(person.get());
+        }
+        else{
+            throw new UsernameNotFoundException("User with "+username+" Not Found");
+        }
+        return null;
+    }
 }
