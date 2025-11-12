4ee064d101f0244f411ffaeb339228b9ab63635e
gdarlene <darlyganza@gmail.com>
2025-11-12 07:51:55 +0200

    changed method signature

--- backend/TerraSustain/src/main/java/org/inganji/TerraSustain/service/impl/CustomUserDetailsService.java
+++ backend/TerraSustain/src/main/java/org/inganji/TerraSustain/service/impl/CustomUserDetailsService.java
@@ -1,6 +1,7 @@
 package org.inganji.TerraSustain.service.impl;
 
 import org.inganji.TerraSustain.configuration.CustomUserDetails;
+import org.inganji.TerraSustain.model.DTO.RegisterResponse;
 import org.inganji.TerraSustain.model.Person;
 import org.inganji.TerraSustain.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +17,13 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private PersonRepository personRepo;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<Person> person= personRepo.findByUsernameOrPhoneNumberOrEmail(username);
+    public UserDetails loadUserByUsername(String usernameInput) throws UsernameNotFoundException {
+        Optional<RegisterResponse> person= personRepo.findByUsernameOrPhoneNumberOrEmail(usernameInput);
         if(person.isPresent()){
             return new CustomUserDetails(person.get());
         }
         else{
-            throw new UsernameNotFoundException("User with "+username+" Not Found");
+            throw new UsernameNotFoundException("User with these credentials not found"+usernameInput+" Not Found");
         }
         return null;
     }
