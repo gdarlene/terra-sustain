6f841fc482354a651767402224c62ed7d63f9863
gdarlene <darlyganza@gmail.com>
2025-11-12 07:44:33 +0200

    added custom filtering method

--- backend/TerraSustain/src/main/java/org/inganji/TerraSustain/repository/PersonRepository.java
+++ backend/TerraSustain/src/main/java/org/inganji/TerraSustain/repository/PersonRepository.java
@@ -1,7 +1,11 @@
 package org.inganji.TerraSustain.repository;
 
+import org.inganji.TerraSustain.model.DTO.RegisterResponse;
 import org.inganji.TerraSustain.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    Optional<RegisterResponse> findByUsernameOrPhoneNumberOrEmail(String username, String phoneNumber, String email);
 }
