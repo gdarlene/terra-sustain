da033f2efbf81687d12eab3ed5bacac1356ba296
gdarlene <darlyganza@gmail.com>
2025-11-14 15:54:36 +0200

    modified file

--- backend/TerraSustain/src/main/java/org/inganji/TerraSustain/model/DTO/AuthenticationRequest.java
+++ backend/TerraSustain/src/main/java/org/inganji/TerraSustain/model/DTO/AuthenticationRequest.java
@@ -1,4 +1,13 @@
 package org.inganji.TerraSustain.model.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthenticationRequest {
+    private String username;
+    private String password;
 }
