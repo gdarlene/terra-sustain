afae2d177c9e53d8e653de3e3dcdc54db8d4457a
gdarlene <darlyganza@gmail.com>
2025-11-20 14:24:31 +0200

    added resources page

---
+++ terrasustain/src/components/authenticated-citizen/Resources.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SideBar from "./sidebar";
+import CitizenHeader from "./Citizen-header";
+
+const Resources: React.FC = ()=>{
+return(
+    <div className="min-h-screen bg-[#fcfff7c6]">
+      <SideBar />
+      <CitizenHeader />
+    </div>
+)
+}
+
+export default Resources;
